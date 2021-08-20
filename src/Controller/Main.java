b89c98836b38a74d3c71f77b5ead2b20c30d5ca7
Gabriel de Carvalho Antunes <gabrielca.pro@gmail.com>
2021-08-20 18:44:19 -0300

    Camada de Xadrez e Printando o tabuleiro

--- src/Controller/Main.java
+++ src/Controller/Main.java
@@ -1,11 +1,12 @@
 package Controller;
 
-import BoardGame.Board;
+import Chess.ChessMatch;
 
 public class Main {
 
 	public static void main(String[] args) {
-		Board board = new Board(8, 8);
+		ChessMatch chessMatch = new ChessMatch();
+		UI.printBoard(chessMatch.getPieces());
 	}
 
 }
