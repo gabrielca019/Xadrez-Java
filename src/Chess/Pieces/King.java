14df4860ca901d127c9385fbd01f755cfbfb9f85
Gabriel de Carvalho Antunes <gabrielca.pro@gmail.com>
2021-08-24 23:08:05 -0300

    Implementando movimentos possiveis do rei

--- src/Chess/Pieces/King.java
+++ src/Chess/Pieces/King.java
@@ -1,6 +1,7 @@
 package Chess.Pieces;
 
 import BoardGame.Board;
+import BoardGame.Position;
 import Chess.ChessPiece;
 import Chess.Color;
 
@@ -15,9 +16,50 @@ public class King extends ChessPiece {
 		return "K";
 	}
 
+	private boolean canMove(Position position) {
+		ChessPiece p = (ChessPiece) getBoard().piece(position);
+		return p == null || p.getColor() != getColor();
+	}
+	
 	@Override
 	public boolean[][] possibleMoves() {
 		boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
+		
+		Position p = new Position(0, 0);
+		
+		p.setValues(position.getRow() - 1, position.getColumn()); //pra cima
+		if(getBoard().positionExists(p) && canMove(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+		p.setValues(position.getRow() + 1, position.getColumn()); //pra baixo
+		if(getBoard().positionExists(p) && canMove(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+		p.setValues(position.getRow(), position.getColumn() - 1); //pra esquerda
+		if(getBoard().positionExists(p) && canMove(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+		p.setValues(position.getRow(), position.getColumn() + 1); //pra direita
+		if(getBoard().positionExists(p) && canMove(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+		p.setValues(position.getRow() - 1, position.getColumn() - 1); //pra diagonal superior esquerda
+		if(getBoard().positionExists(p) && canMove(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+		p.setValues(position.getRow() - 1, position.getColumn() + 1); //pra diagonal superior direita
+		if(getBoard().positionExists(p) && canMove(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+		p.setValues(position.getRow() + 1, position.getColumn() - 1); //pra diagonal inferior esquerda
+		if(getBoard().positionExists(p) && canMove(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+		p.setValues(position.getRow() + 1, position.getColumn() + 1); //pra direita inferior direita
+		if(getBoard().positionExists(p) && canMove(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+		
 		return mat;
 	}
 
